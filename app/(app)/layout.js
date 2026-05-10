5d7dca622d572d32b4a60169e949187b13556c32
Vignesh-ops <vigneshvickymailid@gmail.com>
2026-05-10 10:00:36 -0400

    Header fixes layout

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -14,10 +14,11 @@ export default async function AppLayout({ children }) {
     <div className="app-shell">
       <RouteWarmup routes={warmRoutes} />
       <Navbar user={session.user} />
-      <main className="mx-auto min-h-[calc(100vh-64px)] max-w-7xl px-4 py-6 pb-28 sm:px-6 md:pb-6">
+      <main className="mx-auto min-h-[calc(100vh-64px)] max-w-7xl px-4 py-6 pb-28 pt-[env(safe-area-inset-top)] sm:px-6 md:pb-6">
         {children}
       </main>
       <BottomNav role={session.user.role} />
+
     </div>
   );
 }
